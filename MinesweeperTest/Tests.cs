f578c040dacd13e37159f4549f135630c8e76b32
Cindy Cai <cindymxcai@gmail.com>
2020-05-11 16:22:01 +1200

    refactoring for theory

--- MinesweeperTest/Tests.cs
+++ MinesweeperTest/Tests.cs
@@ -6,13 +6,18 @@ namespace MinesweeperTest
 {
     public class ParsingTest
     {
-        [Fact]
-        public void GivenStringInputShouldGetXandY()
+        [Theory]
+        [InlineData("33", 3, 3)]
+        [InlineData("35", 3, 5)]
+        [InlineData("18", 1, 8)]
+
+
+        public void GivenStringInputShouldGetXandY(string input, int expectedX, int expectedY)
         {
             var lineParser = new LineParser();
-            var (x,y) = lineParser.GetSize("3 3");
-            Assert.Equal( 3, x);
-            Assert.Equal( 3, y);
+            var (x,y) = lineParser.GetSize(input);
+            Assert.Equal( expectedX, x);
+            Assert.Equal( expectedY, y);
         }
     }
 }
