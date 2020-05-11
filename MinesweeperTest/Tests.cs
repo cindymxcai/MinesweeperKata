c6f8d1091ab8cff3015396a0feb3433d3479f6bf
Cindy Cai <cindymxcai@gmail.com>
2020-05-11 15:15:37 +1200

    Initialize empty field of given size

---
+++ MinesweeperTest/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace MinesweeperTest
+{
+    public class FieldTest
+    {
+        [Fact]
+        public void SetFieldSizeShouldInitializeEmptyFieldOfGivenSize()
+        {
+            var field = new Field(3,1);
+            Assert.Equal(new List<CellType>(){CellType.Empty, CellType.Empty, CellType.Empty}, field.GetRow(0) );
+        }
+    }
+}
