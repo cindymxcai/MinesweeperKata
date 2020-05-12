44ab089a225c512f502f5ea33c9670dfb6a0019e
Cindy Cai <cindymxcai@gmail.com>
2020-05-12 16:08:17 +1200

    added member data for confidence

--- MinesweeperTest/HintCalculatorTest.cs
+++ MinesweeperTest/HintCalculatorTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection.Emit;
 using Minesweeper;
 using Xunit;
@@ -6,14 +7,26 @@ namespace MinesweeperTest
 {
     public class HintCalculatorTest
     {
-        [Fact]
-        public void ShouldConvertToArrayToFindSurroundingCells()
+        [Theory]
+        [MemberData(nameof(InputData))]
+        public void ShouldConvertToArrayToFindSurroundingCells(string [] input, int rank, int length )
         {
             var hintFieldCalculator = new HintFieldCalculator();
             var fieldCreator = new FieldCreator();
-            fieldCreator.ReadAllFields(new []{ "22","..", "*.", "00"});
+            fieldCreator.ReadAllFields( input);
             var fieldArray = hintFieldCalculator.ConvertToArray(fieldCreator.AllFields[0]);
-            Assert.Equal(2,fieldArray.Rank);
+            Assert.Equal(rank,fieldArray.Rank);
+            Assert.Equal(length,fieldArray.Length);
+        }
+        
+        private static IEnumerable<object[]> InputData()
+        {
+            var test1 = new[] {"33", "...", "...", "...", "00"};
+            var test2 = new[] {"12", "..", "00"};
+            var test3 = new[] {"31", ".", ".","*","00"};
+            yield return new object[] {test1, 2, 9};
+            yield return new object[] {test2, 2, 2};
+            yield return new object[] {test3, 2, 3};
         }
     }
 
