45375179bf9bf378d4eada3ff34f1b596acdb846
Cindy Cai <cindymxcai@gmail.com>
2020-05-12 15:26:06 +1200

    failing all field creating test

--- MinesweeperTest/FieldBuilderTest.cs
+++ MinesweeperTest/FieldBuilderTest.cs
@@ -7,59 +7,50 @@ namespace MinesweeperTest
 {
     public class FieldBuilderTest
     {
-
         [Fact]
         public void FieldCreatorShouldReturnNullIfRowOrColAre0()
         {
             var fieldCreator = new FieldCreator();
-            Assert.Null(fieldCreator.ReadField(new [] {"03", "...", "...", "..."}));
+            Assert.Null(fieldCreator.ReadField(new[] {"03", "...", "...", "..."}));
         }
 
         [Theory]
         [MemberData(nameof(InputData))]
-
-        public void FieldShouldBeMadeOfCorrectSizeGivenValidSize(string [] input, int expectedRowSize, int expectedColSize)
+        public void FieldShouldBeMadeOfCorrectSizeGivenValidSize(string[] input, int expectedRowSize,
+            int expectedColSize)
         {
             var fieldCreator = new FieldCreator();
             var createdField = fieldCreator.ReadField(input);
             Assert.Equal(expectedRowSize, createdField.Row);
             Assert.Equal(expectedColSize, createdField.Col);
         }
-        
+
         private static IEnumerable<object[]> InputData()
         {
-            var test1 = new [] 
-            {
-               "33", "...", "...", "..."
-            };  
-            
-            var test2 = new []
-            {
-                "15", "....."
-            };
-            
-            var test3 =  new []
-            {
-                "21", ".", "."
-            };
-            var test4 = new []
-            {
-                "59", ".........", ".........", ".........",".........", "........."
-            };
-            yield return new object[] { test1, 3, 3 }; 
-            yield return new object[] { test2, 1, 5 }; 
-            yield return new object[] { test3, 2, 1 }; 
-            yield return new object[] { test4, 5, 9}; 
+            var test1 = new[] {"33", "...", "...", "..."};
+            var test2 = new[] {"15", "....."};
+            var test3 = new[] {"21", ".", "."};
+            var test4 = new[] {"59", ".........", ".........", ".........", ".........", "........."};
+            yield return new object[] {test1, 3, 3};
+            yield return new object[] {test2, 1, 5};
+            yield return new object[] {test3, 2, 1};
+            yield return new object[] {test4, 5, 9};
         }
 
-
         [Fact]
-
         public void FieldCreatorShouldThenPopulateFieldAfterCorrectSize()
         {
             var fieldCreator = new FieldCreator();
-            var createdField = fieldCreator.ReadField(new []{"22", "*.", ".."});
-            Assert.Equal(new List<CellType>{CellType.Mine, CellType.Empty}, createdField.GetRow(0));
+            var createdField = fieldCreator.ReadField(new[] {"22", "*.", ".."});
+            Assert.Equal(new List<CellType> {CellType.Mine, CellType.Empty}, createdField.GetRow(0));
+        }
+
+        [Fact]
+        public void FieldCreatorShouldMakeNewFieldIfNumberRead()
+        {
+            var fieldCreator = new FieldCreator();
+            var createdField = fieldCreator.ReadAllFields(new []{"11", ".", "22", "..", "*."});
+            Assert.Equal(2, fieldCreator.ReadAllFields.Count);
         }
     }
 }
