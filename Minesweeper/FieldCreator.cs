14fa9ca12367544859554a358ed3db9f098c5c19
Cindy Cai <cindymxcai@gmail.com>
2020-05-12 15:45:03 +1200

    passing create all fields test and refactor

--- Minesweeper/FieldCreator.cs
+++ Minesweeper/FieldCreator.cs
@@ -1,27 +1,47 @@
+using System.Collections.Generic;
+
 namespace Minesweeper
 {
     public class FieldCreator
     {
-        public Field ReadField(string [] input)
+        public FieldCreator()
+        {
+            AllFields = new List<Field>();
+        }
+
+        public List<Field> AllFields { get; }
+
+        public void ReadAllFields(string[] input)
+        {
+            var index = 0;
+            var currentField = ReadField(input, index);
+            
+            while (currentField != null)
+            {
+                AllFields.Add(currentField);
+                index += currentField.Row + 1;
+                currentField = ReadField(input, index);
+            }
+        }
+
+        public Field ReadField(string[] input, int index)
         {
             var lineParser = new LineParser();
-            var (row,col) = lineParser.GetSize(input[0]);
+            var (row, col) = lineParser.GetSize(input[index]);
             if (row == 0 || col == 0)
             {
                 return null;
             }
 
+            index++;
             var createdField = new Field(row, col);
-
-            var index = 1;
-
             for (var currentRow = 0; currentRow < row; currentRow++)
             {
                 createdField.SetRow(currentRow, lineParser.GetFieldRow(input[index]));
                 index++;
             }
-            return createdField;
 
+            return createdField;
         }
     }
 }
--- MinesweeperTest/FieldBuilderTest.cs
+++ MinesweeperTest/FieldBuilderTest.cs
@@ -11,7 +11,7 @@ namespace MinesweeperTest
         public void FieldCreatorShouldReturnNullIfRowOrColAre0()
         {
             var fieldCreator = new FieldCreator();
-            Assert.Null(fieldCreator.ReadField(new[] {"03", "...", "...", "..."}));
+            Assert.Null(fieldCreator.ReadField(new[] {"03", "...", "...", "...", "00"}, 0));
         }
 
         [Theory]
@@ -20,17 +20,17 @@ namespace MinesweeperTest
             int expectedColSize)
         {
             var fieldCreator = new FieldCreator();
-            var createdField = fieldCreator.ReadField(input);
+            var createdField = fieldCreator.ReadField(input, 0);
             Assert.Equal(expectedRowSize, createdField.Row);
             Assert.Equal(expectedColSize, createdField.Col);
         }
 
         private static IEnumerable<object[]> InputData()
         {
-            var test1 = new[] {"33", "...", "...", "..."};
-            var test2 = new[] {"15", "....."};
-            var test3 = new[] {"21", ".", "."};
-            var test4 = new[] {"59", ".........", ".........", ".........", ".........", "........."};
+            var test1 = new[] {"33", "...", "...", "...", "00"};
+            var test2 = new[] {"15", ".....", "00"};
+            var test3 = new[] {"21", ".", ".", "00"};
+            var test4 = new[] {"59", ".........", ".........", ".........", ".........", ".........", "00"};
             yield return new object[] {test1, 3, 3};
             yield return new object[] {test2, 1, 5};
             yield return new object[] {test3, 2, 1};
@@ -41,7 +41,7 @@ namespace MinesweeperTest
         public void FieldCreatorShouldThenPopulateFieldAfterCorrectSize()
         {
             var fieldCreator = new FieldCreator();
-            var createdField = fieldCreator.ReadField(new[] {"22", "*.", ".."});
+            var createdField = fieldCreator.ReadField(new[] {"22", "*.", "..", "00"}, 0);
             Assert.Equal(new List<CellType> {CellType.Mine, CellType.Empty}, createdField.GetRow(0));
         }
 
@@ -49,7 +49,7 @@ namespace MinesweeperTest
         public void FieldCreatorShouldMakeNewFieldIfNumberRead()
         {
             var fieldCreator = new FieldCreator();
-            var createdField = fieldCreator.ReadAllFields(new []{"11", ".", "22", "..", "*."});
+            fieldCreator.ReadAllFields(new []{"11", ".", "22", "..", "*.", "00"});
             Assert.Equal(2, fieldCreator.AllFields.Count);
         }
     }
