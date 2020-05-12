13b0dc343e587c3f272b35221cdf5c246160d5e4
Cindy Cai <cindymxcai@gmail.com>
2020-05-12 12:55:56 +1200

    passing field size test

--- MinesweeperTest/FieldBuilderTest.cs
+++ MinesweeperTest/FieldBuilderTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection.Emit;
 using Minesweeper;
 using Xunit;
@@ -10,10 +11,27 @@ namespace MinesweeperTest
         [Fact]
         public void FieldShouldReturnNullIfRowOrColAre0()
         {
-            var lineParser = new LineParser();
-            lineParser.GetSize("33");
             var fieldCreator = new FieldCreator();
-            Assert.Null(fieldCreator.ReadField());
+            Assert.Null(fieldCreator.ReadField("00"));
         }
+    }
+
+    public class FieldCreator
+    {
+        public Field ReadField(string input)
+        {
+            var lineParser = new LineParser();
+            var (x,y) = lineParser.GetSize(input);
+            if (x == 0 || y == 0)
+            {
+                return null;
+            }
+
+            return new Field();
+        }
+    }
+
+    public class Field
+    {
     }
 }
