738d8e849dabe4c79ed84abd494182bd30f3ced6
Cindy Cai <cindymxcai@gmail.com>
2020-05-12 16:04:39 +1200

    passing convert to array

--- MinesweeperTest/HintCalculatorTest.cs
+++ MinesweeperTest/HintCalculatorTest.cs
@@ -12,8 +12,19 @@ namespace MinesweeperTest
             var hintFieldCalculator = new HintFieldCalculator();
             var fieldCreator = new FieldCreator();
             fieldCreator.ReadAllFields(new []{ "22","..", "*.", "00"});
-            var fieldArray = hintFieldCalculator.convertToArray(fieldCreator.AllFields[0]);
+            var fieldArray = hintFieldCalculator.ConvertToArray(fieldCreator.AllFields[0]);
             Assert.Equal(2,fieldArray.Rank);
+        }
+    }
+
+    public class HintFieldCalculator
+    {
+        public  CellType[,] ConvertToArray(Field readField)
+        {
+            var row = readField.Row;
+            var col = readField.Col;
+            var hintArray = new CellType[row, col];
+            return hintArray;
         }
     }
 }
