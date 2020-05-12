bcc2048257fbcb3dec6cde38ab257a71815e8046
Cindy Cai <cindymxcai@gmail.com>
2020-05-12 16:40:00 +1200

    passing test for in bound cells

--- Minesweeper/HintFieldCalculator.cs
+++ Minesweeper/HintFieldCalculator.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Minesweeper
 {
     public class HintFieldCalculator
@@ -8,6 +10,30 @@ namespace Minesweeper
             var col = readField.Col;
             var hintArray = new CellType[row, col];
             return hintArray;
+        }
+
+        public IEnumerable<(int, int)> FindSurroundingInBoundCells(int currentRow, int currentCol, int rows, int cols)
+        {
+            var inBoundCells = new List<(int,int)>();
+            for (var i = currentRow-1; i <= currentRow+1; i++)
+            {
+                for (var j = currentCol-1; j <= currentCol+1; j++)
+                {
+                    if (i < 0 || i >= rows || j< 0 || j >= cols)
+                    { 
+                    }
+                    else
+                    {
+                        if ((i, j) != (currentRow, currentCol))
+                        {
+                            inBoundCells.Add((i,j));
+
+                        }
+                    }
+                }
+                
+            }
+            return inBoundCells;
         }
     }
 }
